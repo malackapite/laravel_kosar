66b861e17b9a5c037f1470f536d210a6abbcc764
Agnes-milia <ratipotti@gmail.com>
2023-09-12 13:57:04 +0200

    adatbázis szerkezete készen van

--- library/database/migrations/2014_10_12_000000_create_users_table.php
+++ library/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id(); //auto-increment, primary key, id a neve, bigint típusú
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
